8addbaef4ef9afc03d09a5dfb6e22602f5b2d398
github username <github email address>
2019-08-01 18:57:36 -0700

    Built basic signup form and controller

--- app/controllers/library_controller.rb
+++ app/controllers/library_controller.rb
@@ -2,4 +2,8 @@ class LibraryController < ApplicationController
   get '/lib' do
     erb :index
   end
+
+  get '/lib/new' do
+    erb :'/library/new'
+  end
 end
--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,3 +1,23 @@
 class UserController < ApplicationController
-  
+  get '/signup' do
+    erb :'/users/create_user'
+  end
+
+  post '/signup' do
+    #binding.pry
+    if !params.values.any?("")
+      @user = User.create(params)
+      redirect to '/lib/new'
+    else
+      redirect to '/signup'
+    end
+  end
+
+  get '/login' do
+    erb :'/users/login'
+  end
+
+  post '/login' do
+  end
+
 end
