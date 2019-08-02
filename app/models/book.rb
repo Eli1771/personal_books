eca76aa19dc60b5b6887fb8e95feb8192ff1f964
github username <github email address>
2019-08-01 17:11:41 -0700

    Started adding models to make many to many relationships between authors/books and topics/books

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,3 +1,7 @@
 class Book < ActiveRecord::Base
   belongs_to :case
+  has_many :book_authors
+  has_many :book_topics
+  has_many :authors, through: :book_authors
+  has_many :topics, through: :book_topics
 end
